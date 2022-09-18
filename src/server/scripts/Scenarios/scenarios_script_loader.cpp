cbdcbcb089ad99a2ae9f3fa3d1f10def4be1aec8
fuzionproject <75014882+fuzionproject@users.noreply.github.com>
2022-09-18 11:46:34 +0300

    Update source!

---
+++ src/server/scripts/Scenarios/scenarios_script_loader.cpp
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2017-2019 AshamaneProject <https://github.com/AshamaneProject>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+void AddSC_stormwind_extraction();
+void AddSC_scenario_stormwind_extraction();
+//void AddSC_scenario_the_battle_for_lordaeron();
+//void AddSC_scenario_scenario_the_defense_of_karabor();
+//Void AddSC_scenario_verdant_wilds();
+
+void AddScenariosScripts()
+{
+    AddSC_stormwind_extraction();
+    AddSC_scenario_stormwind_extraction();
+    //AddSC_scenario_the_battle_for_lordaeron();
+    //AddSC_scenario_scenario_the_defense_of_karabor();
+    //AddSC_scenario_verdant_wilds();
+}

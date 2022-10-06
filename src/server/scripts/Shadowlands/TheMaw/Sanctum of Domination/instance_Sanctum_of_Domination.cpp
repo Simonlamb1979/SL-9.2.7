ecab1794800165f2d0ce9f6c2a9617b773c34b75
fuzionproject <75014882+fuzionproject@users.noreply.github.com>
2022-10-06 12:27:25 +0300

    Update instance_Sanctum_of_Domination.cpp

--- src/server/scripts/Shadowlands/TheMaw/Sanctum of Domination/instance_Sanctum_of_Domination.cpp
+++ src/server/scripts/Shadowlands/TheMaw/Sanctum of Domination/instance_Sanctum_of_Domination.cpp
@@ -30,44 +30,7 @@ Script Data End */
 #include "GameObject.h"
 #include "GameObjectAI.h"
 #include "Sanctum_of_Domination.h"
-
-enum Objects
-{
-    Anvil = 192628,
-    Domination-Etched Treasure Cache = 369898,
-    Gold_Coins = 235985,
-    Snow_Patch = 358375,
-    Xyraths_Booby-Trapped Cache = 375536,
-};
-
-enum Quests
-{
-    Blingtron_7000 = 56042,
-    Owlcat Soul = 62432,
-    Sanctum of Domination_N - Damned If You Dont = 64597,
-    Sanctum of Domination - Damned If You Dont_H = 64598,
-    Sanctum of Domination - Damned If You Dont_M = 64599,
-    Storming the Sanctum = 63903,
-};
-
-enum Loot
-{
-    Soothsayers_Runes = 71716,
-    Owlcat_Soul = 182177,
-    Runekeepers_Ingot = 187175,
-};
-
-enum Achievements 
-{
-    // Guild achievements
-    Hall_of_Fame: Sylvanas_A = 15197,
-    Hall_of_Fame: Sylvanas_H = 15196,
-    Heroic: Sanctum_of_Domination_Guild_Run = 15183,
-    Mythic: Sanctum_of_Domination_Guild_Run = 15184,
-    Sanctum_of_Domination_Guild_Run = 15182,
-    // Character Achievements
-    Ahead_of_the_Curve: Sylvanas_Windrunner = 15134,
-}
+ 
 // zone  13561
 struct instance_Sanctum_of_Domination : public InstanceScript
 {
